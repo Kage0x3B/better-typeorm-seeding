cd6d6eb6d28e47986c3dcee4b21395f5600eba1c
Moritz Hein <moritz.hein@live.de>
2022-08-05 19:05:45 +0200

    Base library project setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    rules: {
+        'prettier/prettier': 'warn',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        'prefer-const': 'warn',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/prefer-namespace-keyword': 'off'
+    }
+};
